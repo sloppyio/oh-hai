f7500aba182ced9583c8a424012efcb218348686
Marc Zimmermann <marc.zimmermann@mmbash.de>
2017-01-24 19:04:57 +0100

    chore(cli): Remove cli because of coloured output

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,17 @@
 # simple deploy script for sloppy.io platform for travis-ci
 #@author marc@sloppy.io
 
+set -x
+
 apiuri="https://api.sloppy.io/v1"
 headers="Authorization:Bearer $SLOPPY_TOKEN"
 
 status=$(http --ignore-stdin GET $apiuri/apps/$SLOPPY_PROJECT "$headers" | jq -r .status)
 
-if [ $status != "error" ]; then
+if [ "$status" != "error" ]; then
  echo "Change project $SLOPPY_PROJECT"
- sloppy change -var domain:$DOMAIN $SLOPPY_PROJECT $SLOPPY_FILE
+ ./sloppy change -var domain:$DOMAIN $SLOPPY_PROJECT $SLOPPY_FILE
 else
  echo "Start project $SLOPPY_PROJECT"
- sloppy start -var domain:$DOMAIN $SLOPPY_FILE
+ ./sloppy start -var domain:$DOMAIN $SLOPPY_FILE
 fi
