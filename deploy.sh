42c64a4b3990fca97dbeff8671efb91c11cdf3f5
Marc Zimmermann <marc.zimmermann@mmbash.de>
2017-01-24 20:14:00 +0100

    chore(cli): Remove cli because of coloured output

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,17 @@ apiuri="https://api.sloppy.io/v1"
 headers="Authorization:Bearer $SLOPPY_APITOKEN"
 
 status=$(curl -s -XGET -H "Content-Type: application/json" -H "$headers" $apiuri/apps/$SLOPPY_PROJECT | jq -r .status)
+jq --arg TRAVIS_COMMIT "$TRAVIS_COMMIT" ".services[].apps[].image=\"sloppy/frontend:$TRAVIS_COMMIT\"" $SLOPPY_FILE > deploy.json
+
+cat deploy.json
 
 if [ "$status" != "error" ]; then
  echo "Change project $SLOPPY_PROJECT"
- ./sloppy change -var domain:$DOMAIN $SLOPPY_PROJECT $SLOPPY_FILE
+ ./sloppy change -var domain:$DOMAIN $SLOPPY_PROJECT deploy.json
  exit 0
 else
  echo "Start project $SLOPPY_PROJECT"
- ./sloppy start -var domain:$DOMAIN $SLOPPY_FILE
+ ./sloppy start -var domain:$DOMAIN $SLOPPY_FILE deploy.json
  exit 0
 fi
 
